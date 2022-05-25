df2d42f54244070d5bd8b64db39ffc390c25ece6
Gurgen Torosyan <gurgen.torosyan@exio.tech>
2022-05-25 18:00:40 +0400

    Cache

--- cache.go
+++ cache.go
@@ -1,23 +1,61 @@
 package cache
 
-import "time"
+import (
+	"fmt"
+	"time"
+	"errors"
+)
+
+var (
+	errorWrongTime = errors.New("wrong time or format")
+	errorKeyExpired = errors.New("key deadline expired")
+)
 
 type Cache struct {
+	data map[string]string
+	deadline map[string]time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	data := make(map[string]string)
+	deadline := make(map[string]time.Time)
+	return Cache{data, deadline}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+	if c.deadline[key].IsZero() {
+		if c.data[key] != "" {
+			return c.data[key], true
+		} else {
+			return "", false
+		}
+	}
 
+	if  c.deadline[key].After(time.Now()) {
+		return c.data[key], true
+	} else {
+		fmt.Println(fmt.Errorf("%w", errorKeyExpired))
+		return "", false
+	}
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.data[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	var keys []string
+	for k := range c.data {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	if deadline.After(time.Now()) {
+		c.Put(key, value)
+		c.deadline[key] = deadline
+	} else {
+		fmt.Println(fmt.Errorf("%w", errorWrongTime))
+	}
 }
